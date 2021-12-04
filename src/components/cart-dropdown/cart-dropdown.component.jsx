58666f2c7dcbbf20c92b3f8ff8d2a3d50bcd9208
Yasir Shariff <goodwonder5@gmail.com>
2021-12-04 15:01:51 +0300

    fix up

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,9 @@ import "./cart-dropdown.style.scss";
 
 const CartDropdown = () => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
